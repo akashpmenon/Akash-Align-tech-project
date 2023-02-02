93b253ae0a5263ee2bdfc728fa323c3480c6fde9
akashpmenon <akashpmenon0492@gmail.com>
2023-02-02 23:09:17 +0530

    Akash.p.menon | Creating custom react scafold scafold

--- src/Components/Table/Table.js
+++ src/Components/Table/Table.js
@@ -5,6 +5,7 @@ import { Button, Table, Tag } from 'antd';
 const Table = ({ data, columns }) => {
     return (<div className={classes.CustomTable}>
         <Table
+            
             columns={columns}
             data={data}
         />
